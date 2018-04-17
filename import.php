b47d98300a0692d300fda04476d0802e5f608fea
obeaker <obika.mgbemena@gmail.com>
2018-04-17 18:05:54 -0400

    Created Index.php, table.css and import.css

---
+++ import.php
@@ -0,0 +1,48 @@
+<?php
+require "app/Models/Util.php";
+require "app/Models/Person.php";
+require "app/Models/Group.php";
+include 'dbConfig.php';
+
+use App\Models\Util;
+use App\Models\Person;
+use App\Models\Group;
+
+if(isset($_POST['Import'])){
+	$csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values',
+		'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv',
+		'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
+
+	if(!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'],$csvMimes)){
+        if(is_uploaded_file($_FILES['file']['tmp_name'])){
+        	$filename=$_FILES["file"]["tmp_name"];
+
+        	$util = new Util();
+
+        	$util->setColumnCount($filename);
+        	$count = $util->getColumnCount();
+
+        	$file = fopen($filename, "r");
+
+          if($count === 2) {
+            $group = new Group();
+          	$group->dataSaveOrUpdate($file, $db);
+          }
+          else if ($count === 6) {
+            $person = new Person();
+          	$person->dataSaveOrUpdate($file, $db);
+          }
+          else {
+            echo "<script type=\"text/javascript\">
+							alert(\"Invalid File:Please Upload either a Person or Group CSV File.\");
+							window.location = \"index.php\"
+						  </script>";
+          }
+
+
+
+        }
+    }
+}
+
+header("Location: index.php");
---
+++ index.php
@@ -0,0 +1,122 @@
+<?php
+//load the database configuration file
+include 'dbConfig.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+  integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.30.1/js/jquery.tablesorter.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="css/table.css">
+ <script type="text/javascript">
+$(document).ready(function() {
+    $("#persons").tablesorter({widthFixed: true, widgets: ['zebra']}).tablesorterPager({container: $("#pager")});
+});
+ </script>
+
+</head>
+
+<body>
+    <div id="wrap">
+        <div class="container">
+          <div class="row">
+            <p>
+              <h1>Person or Group Import Utility </h1>
+            </p>
+          </div>
+            <div class="row">
+
+                <form class="form-horizontal" action="import.php" method="post" name="upload_excel" enctype="multipart/form-data">
+                  <table id="import" cellspacing="0" cellpadding="0" class="breeze table table-bordered">
+                      <thead>
+                          <tr>
+                            <th></th>
+                            <th></th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>
+                            <label class="col-md-4 control-label" for="filebutton">Select File</label>
+                              <input type="file" name="file" id="file" class="input-large">
+                          </td>
+                          <td> <button type="submit" id="submit" name="Import" class="btn btn-primary button-loading" data-loading-text="Loading...">Import</button> </td>
+                      </tbody>
+                  </table>
+                </form>
+
+            </div>
+            <div class="row">
+              <p>
+                <h4>Imported Persons Table </h4>
+              </p>
+                <table id="persons" cellspacing="0" cellpadding="0" class="breeze table table-bordered">
+                    <thead>
+                        <tr>
+                          <th>Person ID</th>
+                          <th>First Name</th>
+                          <th>Last Name</th>
+                          <th>Email Address</th>
+                          <th>Group ID</th>
+                          <th>State</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                        //get records from database
+                        $query = $db->query("SELECT * FROM persons ORDER BY person_id ASC");
+                        if($query->num_rows > 0){
+                            while($row = $query->fetch_assoc()){ ?>
+                        <tr>
+                          <td><?php echo $row['person_id']; ?></td>
+                          <td><?php echo $row['first_name']; ?></td>
+                          <td><?php echo $row['last_name']; ?></td>
+                          <td><?php echo $row['email_address']; ?></td>
+                          <td><?php echo $row['group_id']; ?></td>
+                          <td><?php echo $row['state']; ?></td>
+                        </tr>
+                        <?php } }else{ ?>
+                        <tr><td colspan="6">No person(s) found.....</td></tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="row">
+              <p>
+                <h4>Imported Groups Table </h4>
+              </p>
+                <table id="Groups" cellspacing="0" cellpadding="0" class="breeze table table-bordered">
+                    <thead>
+                        <tr>
+                          <th>Group ID</th>
+                          <th>Group Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                        //get records from database
+                        $query = $db->query("SELECT * FROM groups ORDER BY group_id ASC");
+                        if($query->num_rows > 0){
+                            while($row = $query->fetch_assoc()){ ?>
+                        <tr>
+                          <td><?php echo $row['group_id']; ?></td>
+                          <td><?php echo $row['group_name']; ?></td>
+                        </tr>
+                        <?php } }else{ ?>
+                        <tr><td colspan="2">No group(s) found.....</td></tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
